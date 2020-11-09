ca099271fc1f5533b00906e9e44062e78068a589
greg3d <greg3d@hotbox.ru>
2020-11-09 12:43:59 +0300

    Initial Commit

---
+++ Der Bank App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Der Bank App
+//
+//  Created by Greg3D on 09/11/2020.
+//  Copyright © 2020 Greg3D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
