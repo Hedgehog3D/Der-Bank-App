5d7fd51f3218f7684b60f1e6dcd7d36242608afb
greg3d <greg3d@hotbox.ru>
2020-11-10 12:59:10 +0300

    add some code

--- Der Bank App/ViewController.swift
+++ Der Bank App/ViewController.swift
@@ -6,15 +6,44 @@
 //  Copyright © 2020 Greg3D. All rights reserved.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var value_Label: UILabel!
+//    @IBOutlet weak var value_Label: UILabel = 0;
+    @IBOutlet weak var value_Label: UILabel?
+    
+    var value = 1000.00 {
+        // добавление наблюдателя
+        didSet{
+            updateValue()
+        }
+    }
+    
+    func updateValue(){
+        value_Label?.text = "Баланс: \(value)"
+    }
+    
+//    var value: Int!
+//    var value: Int?
+//    var value: Int
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // В случае ошибки, эта строка не выполнится. Если использовать ! - будет ошибка РанТайм ... ? используется для элементов, которые НЕ отображаются на экране
+//        value_Label?.text = "Der text";
+//        value_Label?.text = String(value)
+        // интерполяция ... можно использовать выражения
+        //value_Label?.text = "Баланс: \(value)"
+        updateValue()
+        value += 1
     }
 
 
+    
 }
 
